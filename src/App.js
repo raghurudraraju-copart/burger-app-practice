656123c932263c7c5c278dfc262ef5345adacde9
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-03-02 01:29:16 +0530

    Accessing methods using props and firing the event from stateless component.

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
         <h1>Welcome to Burger App</h1>
         <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = (props) => {
   return <div>
-    <p>Im {props.name}! i am {props.age}</p>
+    <p onClick={props.click}>Im {props.name}! i am {props.age}</p>
     <p>{props.children}</p>
   </div>
 }
