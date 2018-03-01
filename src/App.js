f92f7edda297c2fb1dd2a4dd960fc0803bc54fc5
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-03-01 23:43:12 +0530

    Replaced hardcoded data with state values.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Raghu', age: 29 },
+      { name: 'Varma', age: 29 },
+      { name: 'RRV', age: 29 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to Burger App</h1>
-        <Person name="Raghu" age="29"/>
-        <Person name="Varma" age="29">My Hobbies are playing cricket, listening music</Person>
-        <Person name="RRV" age="29"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to Burger App'));
