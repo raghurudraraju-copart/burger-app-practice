65d05f3f610a03814688fd31fb6804a176d1a608
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-03-02 01:35:32 +0530

    Accessing methods using bind method and arrow functions

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('Switch Name button Was clicked.');
     this.setState({persons: [
-      { name: 'Raghu Varma', age: 29 },
+      { name: newName, age: 29 },
       { name: 'Varma', age: 29 },
       { name: 'RRV', age: 29 }
     ]
@@ -25,9 +25,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome to Burger App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler("RRV") }>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "Raghu Varma R")}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
