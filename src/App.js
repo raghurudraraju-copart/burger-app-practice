aa133a5a35682686bf8de1ea94710cf861731c9a
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-03-01 13:22:51 +0530

    Added Static react component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Welcome to Burger App</h1>
+        <Person />
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to Burger App'));
   }
 }
 
