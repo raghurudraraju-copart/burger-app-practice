5d45c1ed69ef75880db6c478dd44d7cd775bcca2
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-03-03 14:14:36 +0530

    Refactoring code with adding map for common code.

--- src/App.js
+++ src/App.js
@@ -50,9 +50,13 @@ class App extends Component {
     if( this.state.showPersons) {
       persons = (
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} nameChanged={this.nameChangeHandler}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "Raghu Varma R")}/>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          {
+          this.state.persons.map( (person, key) => {
+            return <Person key={key} name={person.name} age={person.age} nameChanged={this.nameChangeHandler}/>
+          }
+
+          )
+          }
         </div>
       )
     }
