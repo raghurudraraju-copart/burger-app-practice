7b2b23d346ae5a538aa30e14457e758b07e1ea3e
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-03-05 12:49:15 +0530

    Updating state immutably.

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ class App extends Component {
   // }
 
   deletePersonHandler = (personIndex) => {
-      const persons = this.state.persons;
+      //const persons = this.state.persons;
+      //const persons = this.state.persons.slice();//It will give the copy of the array not the refernced value.
+      const persons = [...this.state.persons];
       persons.splice(personIndex, 1);
       this.setState({persons: persons});
     };
